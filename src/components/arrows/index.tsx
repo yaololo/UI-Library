411bd3fbb204dbfe1d546603aa813d6eaee17bd3
Zhuang <yozhuan@capel0070368368.sg128.corpintra.net>
2020-07-17 09:07:35 +0800

    Feature - Add arrows component

---
+++ src/components/arrows/index.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledDownArrow = styled.i`
+  border: solid white;
+  border-width: 0 2px 2px 0;
+  display: inline-block;
+  padding: 4px;
+  cursor: pointer;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+`;
+
+export const StyledRightArrow = styled.i`
+  border: solid white;
+  border-width: 0 2px 2px 0;
+  display: inline-block;
+  padding: 4px;
+  cursor: pointer;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+`;
